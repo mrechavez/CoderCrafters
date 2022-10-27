5795e4f2b9d5d8f4083d0fa91e57838c7930ee4f
mrechavez <milesechavez@gmail.com>
2022-10-26 20:27:35 -0400

    replaced contact header image

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import Header from '../../components/Header'
-import HeaderImage from '../../images/header_bg_2.jpg'
+import HeaderImage from '../../images2/header_bg_2.jpg'
 import {MdEmail} from 'react-icons/md'
 import {BsMessenger} from 'react-icons/bs'
 import {IoLogoWhatsapp} from 'react-icons/io'
